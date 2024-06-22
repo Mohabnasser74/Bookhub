e9864ef2644fbc462538fb03b289b23f12d3db09
Mohab <mohabnasser901@gmail.com>
2024-06-22 13:56:47 +0300

    main

--- frontend/src/components/profile/Repos.jsx
+++ frontend/src/components/profile/Repos.jsx
@@ -68,7 +68,7 @@ const Repos = ({ user, isUserFound, reposUrl, reposCount }) => {
 
   return (
     <div>
-      {isUserFound && !loading ? (
+      {isUserFound ? (
         repositories.length <= 0 ? (
           <div className="text-3xl font-bold text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             {user.user?.login === username ? (
